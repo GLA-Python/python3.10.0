6c8bbea0b2fbd1330c77a23ffbbeddd06fec0ff6
amirkhan1092 <amirkhan1092@gmail.com>
2021-10-22 09:48:46 +0530

    number_sum

---
+++ Basic Programming/sum_number.py
@@ -0,0 +1,10 @@
+# input section
+a = int(input('Enter the first number '))
+b = int(input('Enter yhe second number '))
+
+# logic section
+c = a + b
+
+
+# output section
+print("Sum of two number is", c)
