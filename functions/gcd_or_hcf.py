7afbc84d41bfd597bc619a493a11b88e2c7b7633
amirkhan1092 <amirkhan1092@gmail.com>
2021-12-07 19:33:35 +0530

    gcd and lcm function

---
+++ functions/gcd_or_hcf.py
@@ -0,0 +1,12 @@
+# gcd 
+
+def gcd(num1, num2):
+    pass
+    # logic here and return 
+
+
+# main
+n1 = int(input('Enter the first number '))
+n2 = int(input('Enter the second number '))
+out = gcd(n1, n2)
+print(out)
